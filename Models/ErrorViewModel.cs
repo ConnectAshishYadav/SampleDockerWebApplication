e4dfa55f45b43af46b3e30f0d7bfdca3608cfbe5
connect.ashishyadav <connect.ashishyadav@gmail.com>
2020-12-08 16:45:58 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleDockerWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
